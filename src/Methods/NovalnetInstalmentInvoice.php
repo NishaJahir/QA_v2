35b6c78684094034a58846064b7fc81824b8ff83
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-02-04 16:56:07 +0530

    Update NovalnetInstalmentInvoice.php

--- src/Methods/NovalnetInstalmentInvoice.php
+++ src/Methods/NovalnetInstalmentInvoice.php
@@ -85,9 +85,9 @@ class NovalnetInstalmentInvoice extends PaymentMethodBaseService
                 $instalmentPaymentMimimumAmount = false;
             }
             $this->getLogger(__METHOD__)->error('instalment min amount', $instalmentPaymentMimimumAmount);
-            $paymentConditionValidation = $this->paymentService->checkPaymentDisplayConditions($this->basket, 'novalnet_instalment_invoice');
-            $this->getLogger(__METHOD__)->error('instalment condition', $paymentConditionValidation);
-            return (bool)($this->paymentService->isPaymentActive($this->basket, 'novalnet_instalment_invoice') && $instalmentPaymentMimimumAmount && $paymentConditionValidation);
+           // $paymentConditionValidation = $this->paymentService->checkPaymentDisplayConditions($this->basket, 'novalnet_instalment_invoice');
+           // $this->getLogger(__METHOD__)->error('instalment condition', $paymentConditionValidation);
+            return (bool)($this->paymentService->isPaymentActive($this->basket, 'novalnet_instalment_invoice') && $instalmentPaymentMimimumAmount);
         }
         return false;
     }
