6c0f78861042df64f9d950e5cb4b960229ce2431
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-02-07 19:46:46 +0530

    Update TransactionService.php

--- src/Services/TransactionService.php
+++ src/Services/TransactionService.php
@@ -51,7 +51,7 @@ class TransactionService
             $transaction->saveOneTimeToken      = !empty($transactionData['save_card_token']) ? $transactionData['save_card_token'] : "";
             $transaction->maskingDetails      = !empty($transactionData['mask_details']) ? $transactionData['mask_details'] : null;
             $transaction->instalmentInfo      = !empty($transactionData['instalment_info']) ? $transactionData['instalment_info'] : null;
-            
+            $this->getLogger(__METHOD__)->error('db', $transaction);
             $database->save($transaction);
         } catch (\Exception $e) {
             $this->getLogger(__METHOD__)->error('Novalnet transaction table insert failed!.', $e);
