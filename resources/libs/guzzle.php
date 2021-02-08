335bf7f6cd4ef04837168be41e6ebe0e591fffc3
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-02-08 11:41:35 +0530

    Update guzzle.php

--- resources/libs/guzzle.php
+++ resources/libs/guzzle.php
@@ -1,6 +1,6 @@
 <?php
 
-$client = new \GuzzleHttp\Client();
+$client = new \GuzzleHttp\Client(SdkRestApi::getParam('nn_header'));
 $response = $client->request(
     'POST', 
     SdkRestApi::getParam('nn_request_process_url'), 
